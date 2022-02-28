77f18d9930191423d991988983f7dee8378c3a4a
Akash <akashshekara1212@gmail.com>
2022-02-28 15:15:10 -0600

    fixed issue in index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express"
 import {amount as cps_amount, pay as cps_pay} from './cpsenergy.js'
 import {amount as saws_amount, pay as saws_pay} from './saws.js'
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 const app = express()
 
 app.get("/saws_amount", async (req, res) => {
@@ -14,11 +14,9 @@ app.get("/saws_amount", async (req, res) => {
 })
 
 app.get("/cps_amount", (req, res) => {
-	const response = await saws_amount('001021658-0721990-0003', '78023')
-	res.send("")
+	// const response = await saws_amount('001021658-0721990-0003', '78023')
+	res.send("hello")
 })
-
-app.get("saws")
 
 app.listen(PORT, function () {
   console.log(`Express server listening on port ${PORT}`)
