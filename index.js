1ba8b6337dcfdd34c02108b2f7001bd9745acaef
Akash <akashshekara1212@gmail.com>
2022-03-03 15:36:06 -0600

    please work lol

--- index.js
+++ index.js
@@ -6,13 +6,21 @@ const PORT = process.env.PORT || 3000
 const app = express()
 
 app.get("/cps_amount", async (req, res) => {
-	const response = await cps_amount(req.query.username, req.query.password)
-	res.status(200).json(response)
+	try{
+		const response = await cps_amount(req.query.username, req.query.password)
+		res.status(200).json(response)
+	}catch{
+		res.status(400)
+	}
 })
 
 app.get("/saws_amount", async (req, res) => {
-	const response = await saws_amount(req.query.account, req.query.zip_code)
-	res.status(200).json(response)
+	try{
+		const response = await saws_amount(req.query.account, req.query.zip_code)
+		res.status(200).json(response)
+	}catch{
+		res.status(400)
+	}
 })
 
 app.listen(PORT, function () {
