4e4960ef77bab38b8c9e056ad20965c7a60dd5ee
Akash <akashshekara1212@gmail.com>
2022-03-13 01:18:04 -0600

    license key gumroad

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express"
 import {amount as cps_amount, pay as cps_pay} from './cpsenergy.js'
 import {amount as saws_amount, pay as saws_pay} from './saws.js'
 import path from 'path'
+import axios from 'axios'
 import {parse_url} from './dcl2glb.js'
 const __dirname = path.resolve();
 const PORT = process.env.PORT || 3000
@@ -39,6 +40,30 @@ app.get("/model", async (req, res) => {
 	const response = await parse_url(req.query.url)
 	res.status(200).json(response);
 })
+
+app.get("/validator", async (req, res) => {
+	const license_key = req.query.license_key;
+	const data = {
+	  product_permalink: "yeakza",
+		license_key: license_key,
+	};
+
+	try{
+		const response = await axios.post("https://api.gumroad.com/v2/licenses/verify", 
+			data, 
+			{
+	    	headers: {
+	      	Accept: "application/json",
+	      	"Content-Type": "application/json;charset=UTF-8",
+	    	},
+			},
+		);
+		res.status(200).send("success");
+	}catch(error){
+		res.status(400).send("not valid license key");
+	}
+})
+
 
 app.get("/billing", (req, res) => {
 	res.sendFile(path.join(__dirname, 'public/billing.html'))
--- public/dist/script.js
+++ public/dist/script.js
@@ -1,3 +1,38 @@
+async function validator(license_key){
+  try{
+    const response = await fetch('/validator?license_key=' + license_key);
+    if(response.status == '200'){
+      return true
+    }else{
+      return false
+    }
+  }catch(error){
+    return false
+  }
+}
+
+async function get_model(url){
+  try{
+    const response = await fetch('/model?url=' + url);
+    const json_response = await response.json();
+    return json_response;
+  }catch(error){
+    return false;
+  }
+}
+
+function downloadURI(uri, name) 
+{
+    var link = document.createElement("a");
+    // If you don't know the name or want to use
+    // the webserver default set name = ''
+    link.setAttribute('download', name);
+    link.href = uri;
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+}
+
 window.addEventListener("load", function () {
   console.log(document.querySelector("#showMenu"));
 
@@ -5,18 +40,41 @@ window.addEventListener("load", function () {
     .querySelector("#preview")
     .addEventListener("click", function(event) {
       const url = document.querySelector("#preview_url").value;
-
-      let response_url = null;
+      const license_key = document.querySelector("#license_key").value;
 
       (async() => {
-        const response = await fetch('/model?url=' + url);
-        const model_url = await response.json();
-        BabylonViewer.viewerManager.getViewerPromiseById('model_preview').then(function (viewer) {
-          viewer.loadModel({
-            url: model_url
-          });
-        });
+        const valid = await validator(license_key);
+
+        if(valid){
+          const model_url = await get_model(url);
+          console.log(model_url)
+          if(model_url){
+            BabylonViewer.viewerManager.getViewerPromiseById('model_preview').then(function (viewer) {
+              viewer.loadModel({
+                url: model_url
+              });
+            });
+          }
+        }
       })()
+    }
+  );
+
+  document
+    .querySelector("#download")
+    .addEventListener("click", function(event) {
+      const url = document.querySelector("#preview_url").value;
+      const license_key = document.querySelector("#license_key").value;
+
+      (async() => {
+        const valid = await validator(license_key);
+
+        if(valid){
+          const model_url = await get_model(url);
+          downloadURI(model_url,'')
+        }
+      })()
+
     }
   );
 
